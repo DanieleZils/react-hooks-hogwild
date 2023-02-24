3d9f2038a8cc4e5dab53f6c63b9cfc10a2c14794
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-02-24 09:57:54 -0500

    first thoughts

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,15 @@ import Nav from "./Nav";
 import hogs from "../porkers_data";
 
 function App() {
+	const eachHog = hogs.map((hog) =>
+	<Hog key={hog.name} name={hog.name} specialty={hog.specialty} greased={hog.greased} weight={hog.weight} image={hog.image} />
+	)
+  
+
 	return (
 		<div className="App">
-			<Nav />
+		 	<Nav />
+			{eachHog}
 		</div>
 	);
 }
---
+++ src/components/Hogs.js
@@ -0,0 +1,15 @@
+import React from "react";
+import hogs from "../porkers_data";
+
+
+function Hogs() {
+  
+
+	return (
+		<div>
+			
+		</div>
+	);
+}
+
+export default Hogs;
